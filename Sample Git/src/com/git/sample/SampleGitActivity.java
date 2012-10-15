592f5f87bbc099bb4f3b7a29cb22b1eb66ce326a
krasystem <androidics@gmail.com>
2012-10-15 16:12:57 +0530

    Second Commit

--- Sample Git/src/com/git/sample/SampleGitActivity.java
+++ Sample Git/src/com/git/sample/SampleGitActivity.java
@@ -2,6 +2,7 @@ package com.git.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class SampleGitActivity extends Activity {
         launch.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View v) {
+				Log.d("GIT HUB","Initial Commit");
 				Toast.makeText(SampleGitActivity.this, "Sample GIT Repo", 0).show();
 			}
 		});
